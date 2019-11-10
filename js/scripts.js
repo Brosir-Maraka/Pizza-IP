d2dbc8ac5bab33090c7b74c9787d00ddd2106acc
Brosir-Maraka <brosirm@gmail.com>
2019-11-10 14:08:00 +0300

    add aboutus page

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $(".hide-aboutus, .show-aboutus").click(function() {
+      $(".show-aboutus").toggle();
+      $(".hide-aboutus").toggle();
+    });
+    $(".hide-menu, .show-menu").click(function() {
+      $(".show-menu").toggle();
+      $(".hide-menu").toggle();
+    });
+    $(".hide-services, .show-services").click(function() {
+      $(".show-services").toggle();
+      $(".hide-services").toggle();
+    });
+  }); 
